8e4ad47b0eb1912b346037c8cef527bd29778e07
daniell395 <eda.ni@outlook.com>
2025-03-21 02:20:00 -0500

    Generates test for http request of get all the cashCards, additional add the pagination and sorts

--- cashcard/src/main/java/com/example/cashcard/CashCardRepository.java
+++ cashcard/src/main/java/com/example/cashcard/CashCardRepository.java
@@ -1,10 +1,13 @@
 package com.example.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-interface CashCardRepository extends CrudRepository <CashCard,Long>{
+interface CashCardRepository extends CrudRepository <CashCard,Long>,
+        PagingAndSortingRepository<CashCard, Long> {
+
 
     @RestController
     @RequestMapping("/cashcards")
